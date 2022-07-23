b0aaa859a54da8a03985bbd1f062425c4cfbbd24
stjmbeck <jeremypaul.becker@student.hs-anhalt.de>
2022-07-23 02:46:55 +0200

    All Activities Created - not finished.

--- app/src/main/java/com/hsa/dinder/TitleScreen.java
+++ app/src/main/java/com/hsa/dinder/TitleScreen.java
@@ -2,7 +2,12 @@ package com.hsa.dinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TitleScreen extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class TitleScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
+    }
+    public void to_login(View view) {
+        Intent login = new Intent(view.getContext(), Login.class);
+        startActivity(login);
     }
 }
