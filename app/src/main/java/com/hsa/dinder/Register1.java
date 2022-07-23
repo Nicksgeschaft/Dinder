31e3b479de80c46f3ee8a0ba6c0f27f3a8f5aa98
stjmbeck <jeremypaul.becker@student.hs-anhalt.de>
2022-07-23 02:04:20 +0200

    Initial commit

---
+++ app/src/main/java/com/hsa/dinder/Register1.java
@@ -0,0 +1,14 @@
+package com.hsa.dinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register1);
+    }
+}
