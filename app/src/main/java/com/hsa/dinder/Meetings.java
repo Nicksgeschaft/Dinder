c66dcb6d2adefa4916bd46087582019ab7f354ad
stjmbeck <jeremypaul.becker@student.hs-anhalt.de>
2022-08-18 00:17:03 +0200

    Database Connection Try - Catch

---
+++ app/src/main/java/com/hsa/dinder/Meetings.java
@@ -0,0 +1,165 @@
+package com.hsa.dinder;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Meetings extends AppCompatActivity {
+
+    CheckBox enable_bt, visible_bt;
+    ImageView search_bt;
+    TextView name_bt;
+    ListView listView;
+
+
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice> pairedDevices;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meetings);
+
+        enable_bt = findViewById(R.id.enable_bt);
+        visible_bt = findViewById(R.id.visible_bt);
+        search_bt = findViewById(R.id.search_bt);
+        name_bt = findViewById(R.id.name_bt);
+        listView = findViewById(R.id.list_view);
+
+        name_bt.setText(getLocalBluetoothName());
+
+        BA = BluetoothAdapter.getDefaultAdapter();
+
+
+        if (BA == null) {
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        if (BA.isEnabled()) {
+            enable_bt.setChecked(true);
+        }
+
+        enable_bt.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (!isChecked) {
+                    if (ActivityCompat.checkSelfPermission(Meetings.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    BA.disable();
+                    Toast.makeText(Meetings.this, "Turned off", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intentOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intentOn, 0);
+                    Toast.makeText(Meetings.this, "Turned on", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        visible_bt.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    if (ActivityCompat.checkSelfPermission(Meetings.this, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    startActivityForResult(getVisible, 0);
+                    Toast.makeText(Meetings.this, "Visible for 2 min", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        search_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                list();
+            }
+
+
+        });
+    }
+
+    private void list() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        pairedDevices = BA.getBondedDevices();
+
+        ArrayList list = new ArrayList();
+
+        for (BluetoothDevice bt : pairedDevices) {
+            list.add(bt.getName());
+        }
+
+        Toast.makeText(this, "Showing Devices", Toast.LENGTH_SHORT).show();
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+    }
+
+    public String getLocalBluetoothName() {
+        if (BA == null) {
+            BA = BluetoothAdapter.getDefaultAdapter();
+
+        }
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return null;
+        }
+        String name = BA.getName();
+        if (name==null){
+            name = BA.getAddress();
+        }
+        return name;
+    }
+
+
+}
